404522f341e41453749699a2b1325ee188c929c7
dhawal <galadhawal512@gmail.com>
2025-10-16 10:13:00 +0530

    making routes and restructure listings .

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const ExpressError = require("./utils/ExpressError.js");
 const { ListingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 const { wrap } = require("module");
+const listings = require("./routes/listing.js");
 
 app.engine("ejs", ejsMate);
 app.use(methodOverride("_method"));
@@ -35,16 +36,7 @@ app.get("/", (req, res) => {
   res.send("hi I am root");
 });
 
-const validateListing = (req, res, next) => {
-  let { error } = ListingSchema.validate(req.body); //listing schmea joi wala, usme ham req.body ka data dale check krne ke like ki woh sahi hai ki hai
-  if (error) {
-    // listing schema do jagaha defined hai ek mongodb ke liye dusra mongo db ka data check krne ke liye thorugh joi ejs
-    let errMsg = error.details.map((el) => el.message).join(","); // total msg ke array meh se ek ek msg seprate karega then add karega usko
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
+app.use("/listings", listings);
 
 const validateReview = (req, res, next) => {
   let { error } = reviewSchema.validate(req.body);
@@ -56,74 +48,6 @@ const validateReview = (req, res, next) => {
     next();
   }
 };
-
-// new (upr isliye taki id ke taraha treat na ho /new {show route refrence})
-app.get("/listings/new", (req, res) => {
-  res.render("listings/new.ejs");
-});
-
-//show route
-app.get(
-  "/listings/:id",
-  WrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews"); // reviews ko populate krdia taki reviews ka data bhi aa jaye
-    res.render("listings/show.ejs", { listing });
-  })
-);
-
-//create route
-app.post(
-  "/listings",
-  validateListing,
-  WrapAsync(async (req, res, next) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-  })
-);
-
-//edit route
-app.get(
-  "/listings/:id/edit",
-  WrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-  })
-);
-
-//update route
-app.put(
-  "/listings/:id",
-  validateListing,
-  WrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const listingData = { ...req.body.listing };
-    await Listing.findByIdAndUpdate(id, listingData); // id and kya update krna hai (2 parameters mongodb ka code hai)
-    res.redirect(`/listings/${id}`);
-  })
-);
-
-//delete route
-app.delete(
-  "/listings/:id",
-  WrapAsync(async (req, res) => {
-    const { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-    console.log(deletedListing);
-  })
-);
-
-//index route to display all listings
-app.get(
-  "/listings",
-  WrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
-  })
-);
 
 //post review route
 app.post(
---
+++ routes/listing.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+// path ki locatin parent direcrtory se hai isliye ../
+const WrapAsync = require("../utils/WrapAsync");
+const ExpressError = require("../utils/ExpressError.js");
+const { ListingSchema, reviewSchema } = require("../schema.js");
+const Listing = require("../models/listing");
+
+const validateListing = (req, res, next) => {
+  let { error } = ListingSchema.validate(req.body); //listing schmea joi wala, usme ham req.body ka data dale check krne ke like ki woh sahi hai ki hai
+  if (error) {
+    // listing schema do jagaha defined hai ek mongodb ke liye dusra mongo db ka data check krne ke liye thorugh joi ejs
+    let errMsg = error.details.map((el) => el.message).join(","); // total msg ke array meh se ek ek msg seprate karega then add karega usko
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+// new (upr isliye taki id ke taraha treat na ho /new {show route refrence})
+router.get("/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//show route
+router.get(
+  "/:id",
+  WrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews"); // reviews ko populate krdia taki reviews ka data bhi aa jaye
+    res.render("listings/show.ejs", { listing });
+  })
+);
+
+//create route
+router.post(
+  "/",
+  validateListing,
+  WrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
+
+//edit route
+router.get(
+  "/:id/edit",
+  WrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
+
+//update route
+router.put(
+  "/:id",
+  validateListing,
+  WrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const listingData = { ...req.body.listing };
+    await Listing.findByIdAndUpdate(id, listingData); // id and kya update krna hai (2 parameters mongodb ka code hai)
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+//delete route
+router.delete(
+  "/:id",
+  WrapAsync(async (req, res) => {
+    const { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+    console.log(deletedListing);
+  })
+);
+
+//index route to display all listings
+router.get(
+  "/",
+  WrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
+
+module.exports = router;

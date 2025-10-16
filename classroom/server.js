0ca3bafee991b156c7bfea323088d70193d08297
dhawal <galadhawal512@gmail.com>
2025-10-17 00:26:06 +0530

    use of cookie parser

--- classroom/server.js
+++ classroom/server.js
@@ -3,13 +3,28 @@ const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 
+const cookieParser = require("cookie-parser");
+
+// cookieParser is a function that returns middleware — call it here  (hamne jo users ko req bheja yane parent element toh usper se :id ki value nhi aarhi thi isliye humne cookie parser use kiya . Nhi use krte toh undefined aajata)
+app.use(cookieParser()); // to decode (parse) cookie into req.cookies
+
 app.get("/getcookies", (req, res) => {
-  res.cookie("Greet", "hello"); //name value pair Greet -> name
-  // hello -> uski value
-  // Cookie names cannot contain spaces or certain separators. Use a token-like name.
-  res.cookie("MadeInIndia", "Namaste"); // no spaces
+  res.cookie("Greet", "hello");
+  res.cookie("MadeInIndia", "Namaste");
   res.send("send you some cookies");
 });
+
+app.get("/greet", (req, res) => {
+  let { name = "annonymus" } = req.cookies;
+  res.send(`Hi ${name}`);
+});
+
+app.get("/", (req, res) => {
+  // use req.cookies to inspect parsed cookies
+  console.dir(req.cookies);
+  res.send("hey I am root");
+});
+
 app.use("/users", users);
 
 app.use("/posts", posts); // yeh sare routes jo post.js me hai usko use krne k liye
